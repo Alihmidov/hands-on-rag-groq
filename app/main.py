c76f9674112d328947e418d4606f44071e8862a8
Hamidov Ali <hemidoveli893@gmail.com>
2026-07-11 00:55:38 +0400

    feat: update main,py file using FastApi

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from app.routes.api import router 
 
 app = FastAPI()  
+
+@router.get("/health_check", summary="Check server status")
+def health_check():
+    return {"status": "online", "message": "Api is running."}
+
+app.include_router(router)
