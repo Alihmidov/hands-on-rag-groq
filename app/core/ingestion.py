c18d93f756291d857b970f34ade8496f92fc463b
Hamidov Ali <hemidoveli893@gmail.com>
2026-07-10 22:41:24 +0400

    feat: initialize ingestion and update git/docker ignore

---
+++ app/core/ingestion.py
@@ -0,0 +1,33 @@
+import os 
+import fitz 
+from config.settings import settings 
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
+from langchain_huggingface import HuggingFaceEmbeddings 
+from langchain_chroma import Chroma 
+
+def ingest_document(file_name: str):
+    file_path = os.path.join("data","raw",file_name)
+    
+    if not os.path.exists(file_path):
+        return f"Error: {file_path} not found!"
+    
+    doc = fitz.open(file_path)
+    text = "".join([page.get_text() for page in doc])
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = settings.CHUNK_SIZE,
+        chunk_overlap = settings.CHUNK_OVERLAP
+    )
+    
+    chunks = splitter.split_text(text)
+    
+    vectorstore = Chroma.from_texts(
+        texts = chunks,
+        embedding = HuggingFaceEmbeddings(model_name = settings.EMBEDDING_MODEL),
+        persist_directory = settings.CHROMA_PATH 
+    )
+    
+    return f"Success!"
+
+if __name__ == "__main__":
+    print(ingest_document("handson_ml.pdf"))
