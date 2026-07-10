6ded25bab9ee58f47e732c7d5d2787c501d69624
Hamidov Ali <hemidoveli893@gmail.com>
2026-07-11 01:57:28 +0400

    refactor: change title of FastApi

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI 
 from app.routes.api import router 
 
-app = FastAPI()  
+app = FastAPI(title="HANDS-ON RAG GROK API")  
 
 @router.get("/health_check", summary="Check server status")
 def health_check():
