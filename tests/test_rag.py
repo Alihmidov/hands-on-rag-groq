f21aa64870767e2acd228fb6ddda2246c9186f9c
Hamidov Ali <hemidoveli893@gmail.com>
2026-07-11 17:13:16 +0400

    test: update test assertion for ask_bot

--- tests/test_rag.py
+++ tests/test_rag.py
@@ -2,6 +2,6 @@ import pytest
 from app.core.llm_logic import ask_bot
 
 def test_answer():
-    question = "Who is Donald Trump"
+    question = "Who is Donald Trump?"
     response = ask_bot(question)
     assert "sorry" in response.lower()
